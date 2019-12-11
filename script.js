1a7a68dce2e2768411cb4c9ab7c9b5562caf592f
Avery Stahl <averystahl@hotmail.com>
2019-12-11 15:12:42 -0500

    updated js

--- script.js
+++ script.js
@@ -21,4 +21,26 @@ const nth = function(d)
 $("#currentDay").html(today.toLocaleDateString("en-US", dateFormat) + nth());
 
 // Dynamically add blocks to display each hour of the day with space 
+
+// Add rows for each time
+for(i=1; i < 10; i++)
+{
+  var timeRow = $("<div>").attr({class: "row mb-5", id: i}).appendTo(".container");
+}
+
+// Add Time column
+// If the time is between 9-12 then make the 9-12 divs
+for(i = 9; i < 13; i++)
+{
+  for(j = 1; j < 5; j++)
+  {
+  $("<div>").attr({id: i, class: "col m-2"}).text(i + " a.m.").appendTo(timeRow.attr("id", j));
+  }
+  for(j = 5; j < 13; j++)
+{
+  $("<div>").text(i + " p.m.").attr({id: i, class: "col m-2"}).appendTo(".container");
+}
+}
+
+
 // for user input and a save button.
