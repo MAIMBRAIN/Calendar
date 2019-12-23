95ba0767240a4c310ba014a7952805b700adc461
Avery Stahl <averystahl@hotmail.com>
2019-12-23 11:18:18 -0500

    Added time blocks, data saved to local storage

--- script.js
+++ script.js
@@ -18,29 +18,55 @@ const nth = function(d)
     }
 }
 
+// Today's current date
 $("#currentDay").html(today.toLocaleDateString("en-US", dateFormat) + nth());
 
-// Dynamically add blocks to display each hour of the day with space 
+// Updates the page's current hour
+function hourUpdater() {
+  // Get the current hour
+  var currentHour = moment().hours();
 
-// Add rows for each time
-for(i=1; i < 10; i++)
-{
-  var timeRow = $("<div>").attr({class: "row mb-5", id: i}).appendTo(".container");
+  // Loops over each time block
+  $(".time-block").each(function() {
+    var hourBlock = parseInt($(this).attr("id").split("-")[1]);
+
+    // check if we've moved past this time
+    if (hourBlock < currentHour) {
+      $(this).addClass("past");
+    } 
+    else if (hourBlock === currentHour) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    } 
+    else {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    }
+  });
 }
+hourUpdater();
 
-// Add Time column
-// If the time is between 9-12 then make the 9-12 divs
-for(i = 9; i < 13; i++)
-{
-  for(j = 1; j < 5; j++)
-  {
-  $("<div>").attr({id: i, class: "col m-2"}).text(i + " a.m.").appendTo(timeRow.attr("id", j));
-  }
-  for(j = 5; j < 13; j++)
-{
-  $("<div>").text(i + " p.m.").attr({id: i, class: "col m-2"}).appendTo(".container");
-}
-}
+// Check time if it needs to be updated
+var interval = setInterval(hourUpdater, 15000);
 
+// listen for save button clicks
+$(".saveBtn").on("click", function() {
+  // get nearby values
+  var value = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
 
-// for user input and a save button.
+  // save in localStorage
+  localStorage.setItem(time, value);
+});
+
+// Load saved data from localStorage
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
